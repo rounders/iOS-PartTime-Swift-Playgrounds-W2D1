1953087800d1bc7bd93bffdfebad4d4f1fc5d5ec
Francois Harbec <fharbec@gmail.com>
2017-04-18 21:07:02 -0700

    update code to work in swift3
    when calling a function, swift3 requires that
    the first parameter be named unless the function
    definition uses _ as the external parameter label.

--- W2D1-Functions.playground/Contents.swift
+++ W2D1-Functions.playground/Contents.swift
@@ -41,16 +41,16 @@ func greet(name: String) {
 
 //: ### Challenge 2
 //:
-//: Now take the code your wrote in the last playground to print out a grammatical description of our bicycleCount, and write a function that takes in an Int representing how many bicycles we have, and prints out a description of how many bicycles we have. You can call the function `pluralize`.
+//: Now take the code your wrote in the last playground to print out a grammatical description of our bicycleCount, and write a function that takes in an argument called 'count' which is of type Int and represents the number of bicycles we have and prints out a description of how many bicycles we have. You can call the function `pluralize`.
 
 
 
 
 // Test it by uncommenting these lines:
 
-// pluralize(1)
-// pluralize(0)
-// pluralize(100)
+// pluralize(count: 1)
+// pluralize(count: 0)
+// pluralize(count: 100)
 
 //: Not only can functions take in parameters, but they can return values. So far we've only written functions that don't return anything, they just print stuff. Here is an example of a function that takes in an Int and returns an Int
 
@@ -74,9 +74,9 @@ var result = increment(input: 10)
 
 // Test it by uncommenting the lines below:
 
-//var str1 = pluralizedString(1)
-//var str2 = pluralizedString(0)
-//var str3 = pluralizedString(100)
+//var str1 = pluralizedString(count: 1)
+//var str2 = pluralizedString(count: 0)
+//var str3 = pluralizedString(count: 100)
 
 
 //: ## Bonus Challenge
@@ -85,7 +85,7 @@ var result = increment(input: 10)
 //:
 //: One way would be to take an input word as well as a count and add "s" to the end of it. Something like:
 
-// pluralizedWord("car", 2) // returns "cars"
+// pluralizedWord(word: "car", 2) // returns "cars"
 
 //: But, since english is a silly language, adding "s" doesn't always make a correct pluralization. Irregularities are common. Think of octopus/octopi, sheep/sheep and man/men.
 //: So let's make it even better, we want a fuction called pluralizedWord that takes in 3 inputs:
@@ -102,11 +102,10 @@ var result = increment(input: 10)
 
 // Test your function by uncommenting:
 
-// pluralizedWord("octopus", pluralNoun: "octopi", count: 1)
-// pluralizedWord("woman", pluralNoun: "women", count: 2)
-// pluralizedWord("apple", pluralNoun: "apples", count: 0)
+// pluralizedWord(singular: "octopus", pluralNoun: "octopi", count: 1)
+// pluralizedWord(singular: "woman", pluralNoun: "women", count: 2)
+// pluralizedWord(singular: "apple", pluralNoun: "apples", count: 0)
 
-// You might have noticed that when we have a function that takes multiple inputs, we have to explicitly write out all input names other than the first one e.g. `pluralNoun` and `count`, seen above.
 
 
 
@@ -117,5 +116,3 @@ var result = increment(input: 10)
 
 
 //: THE END
-
-
